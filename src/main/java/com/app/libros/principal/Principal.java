623a8866e122fe2ee0665dafe53eb1d5488ff0b2
Sebastian Tobito <sebastiantobito@gmail.com>
2024-07-10 11:16:26 -0500

    metodo para ver el top 10 de libros

--- src/main/java/com/app/libros/principal/Principal.java
+++ src/main/java/com/app/libros/principal/Principal.java
@@ -1,17 +1,31 @@
 package com.app.libros.principal;
 
+import com.app.libros.model.Datos;
+import com.app.libros.model.DatosLibros;
 import com.app.libros.service.ConsumoAPI;
 import com.app.libros.service.ConvierteDatos;
 
 import java.net.URL;
+import java.util.Comparator;
 
 public class Principal {
-    private static final String URL_BASE = "https://gutendex.com/books/";
+    private static final String URL_BASE = "https://gutendex.com/books/sarch=";
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos conversorDatos = new ConvierteDatos();
     public void muestraElMenu(){
         var json = consumoAPI.obtenerDatos(URL_BASE);
         System.out.println(json);
+        var datos = conversorDatos.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+        //Top 10 más descargados
+        System.out.println("Top 10");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDescargas).reversed())
+                .limit(10)
+                .map(l->l.titulo().toUpperCase())
+                .forEach(System.out::println);
+        //Busqueda de libros por nombre
     }
+
 
 }
